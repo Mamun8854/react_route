98fca2049281851347a134bcef63623037e73765
Mamun8854 <mamunabdullah8854@gmail.com>
2022-10-11 17:08:59 +0600

    Quiz Question Design modified

--- src/Components/QuizDetails/QuizDetails.js
+++ src/Components/QuizDetails/QuizDetails.js
@@ -8,6 +8,7 @@ import Swal from "sweetalert2";
 
 const QuizDetails = ({ q }) => {
   const { question, correctAnswer, options } = q;
+  console.log(question);
   const handleAnswer = (option, clicked) => {
     if (option === clicked) {
       Swal.fire("Good job!", "Correct Answer!", "success");
@@ -16,7 +17,6 @@ const QuizDetails = ({ q }) => {
         icon: "error",
         title: "Oops...",
         text: "Wrong Answer!",
-        footer: '<a href="">Why do I have this issue?</a>',
       });
     }
   };
@@ -26,16 +26,17 @@ const QuizDetails = ({ q }) => {
         <div className="">
           <h1 className="text-base font-bold mb-6">{question}</h1>
 
-          <div className="text-sm flex flex-col gap-5">
+          <div className="text-sm flex flex-col gap-5 p-6">
             {options.map((option) => (
               <div>
-                <label className="form-check-label  justify-self-start items-center flex md:gap-5 gap-2 text-center">
+                <label className="form-check-label  justify-self-start items-start flex md:gap-5 gap-2 text-left">
                   <input
-                    type="checkbox"
+                    type="radio"
+                    name="radio"
                     className="form-check-input flex-none  md:w-5 md:h-5"
                     onClick={() => handleAnswer(option, correctAnswer)}
                   />
-                  <h1 className="flex-initial items-center md:w-96">
+                  <h1 className="flex-initial items-start md:w-96">
                     {" "}
                     {option}
                   </h1>
